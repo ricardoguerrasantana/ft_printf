3700ae23d3ed6edf0457891b789182f425c719fd
ricardoguerrasantana <ricardo.andres.guerra.santana@gmail.com>
2024-12-15 06:47:41 +0000

    Add unit tests for i specifier printing in ft_printf

---
+++ tests/test_integer.c
@@ -0,0 +1,52 @@
+#include "ft_printf.h"
+#include "test_utils.h"
+
+// Test printing positive integer
+Test(ft_printf, positive_integer, .init = setup)
+{
+    ft_printf("Value: %i\n", 12345);
+    cr_assert_stdout_eq_str("Value: 12345\n");
+}
+
+// Test printing negative integer
+Test(ft_printf, negative_integer, .init = setup)
+{
+    ft_printf("Value: %i\n", -12345);
+    cr_assert_stdout_eq_str("Value: -12345\n");
+}
+
+// Test printing zero integer
+Test(ft_printf, zero_integer, .init = setup)
+{
+    ft_printf("Value: %i\n", 0);
+    cr_assert_stdout_eq_str("Value: 0\n");
+}
+
+// Test printing maximum integer
+Test(ft_printf, max_integer, .init = setup)
+{
+    ft_printf("Value: %i\n", 2147483647);
+    cr_assert_stdout_eq_str("Value: 2147483647\n");
+}
+
+// Test printing minimum integer
+Test(ft_printf, min_integer, .init = setup)
+{
+    ft_printf("Value: %i\n", -2147483648);
+    cr_assert_stdout_eq_str("Value: -2147483648\n");
+}
+
+// Test printing multiple integers
+Test(ft_printf, multiple_integers, .init = setup)
+{
+    ft_printf("Values: %i, %i, %i\n", 1, -2, 3);
+    cr_assert_stdout_eq_str("Values: 1, -2, 3\n");
+}
+
+// Test printing mixed specifiers with integers
+Test(ft_printf, mixed_specifiers_with_integers, .init = setup)
+{
+    ft_printf("Char: %c, String: %s, Value: %i\n", 'A', "Test", 123);
+    cr_assert_stdout_eq_str("Char: A, String: Test, Value: 123\n");
+}
+
