2c5197d9c4f84c982b6406f3701de19c2b80bbdf
ricardoguerrasantana <ricardo.andres.guerra.santana@gmail.com>
2025-01-18 19:08:32 +0000

    Refactor percent sign tests to use sprintf for expected output comparison and validate return lengths

--- tests/test_percent.c
+++ tests/test_percent.c
@@ -4,34 +4,49 @@
 // Test printing a single percent sign
 Test(ft_printf, prints_single_percent, .init = setup)
 {
-    ft_printf("Percent: %%\n");
-    cr_assert_stdout_eq_str("Percent: %\n");
+    char expected[100];
+    int expected_len = sprintf(expected, "Percent: %%\n");
+    int len = ft_printf("Percent: %%\n");
+    cr_assert_stdout_eq_str(expected);
+    cr_assert_eq(len, expected_len);
 }
 
 // Test printing percent signs without any additional text
 Test(ft_printf, prints_percent_alone, .init = setup)
 {
-    ft_printf("%%");
-    cr_assert_stdout_eq_str("%");
+    char expected[100];
+    int expected_len = sprintf(expected, "%%");
+    int len = ft_printf("%%");
+    cr_assert_stdout_eq_str(expected);
+    cr_assert_eq(len, expected_len);
 }
 
 // Test printing multiple percent signs
 Test(ft_printf, prints_multiple_percent, .init = setup)
 {
-    ft_printf("%%%%\n");
-    cr_assert_stdout_eq_str("%%\n");
+    char expected[100];
+    int expected_len = sprintf(expected, "%%%%\n");
+    int len = ft_printf("%%%%\n");
+    cr_assert_stdout_eq_str(expected);
+    cr_assert_eq(len, expected_len);
 }
 
 // Test percent signs with other text
 Test(ft_printf, prints_percent_with_text, .init = setup)
 {
-    ft_printf("Loading%% Complete\n");
-    cr_assert_stdout_eq_str("Loading% Complete\n");
+    char expected[100];
+    int expected_len = sprintf(expected, "Loading%% Complete\n");
+    int len = ft_printf("Loading%% Complete\n");
+    cr_assert_stdout_eq_str(expected);
+    cr_assert_eq(len, expected_len);
 }
 
 // Test percent signs with no additional formatting
 Test(ft_printf, prints_percent_no_formatting, .init = setup)
 {
-    ft_printf("%% %%\n");
-    cr_assert_stdout_eq_str("% %\n");
+    char expected[100];
+    int expected_len = sprintf(expected, "%% %%\n");
+    int len = ft_printf("%% %%\n");
+    cr_assert_stdout_eq_str(expected);
+    cr_assert_eq(len, expected_len);
 }
