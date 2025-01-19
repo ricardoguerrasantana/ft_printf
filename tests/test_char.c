2098e3d031b8df6a6acdd6e0afe66feadc6dab21
ricardoguerrasantana <ricardo.andres.guerra.santana@gmail.com>
2025-01-19 07:35:37 +0000

    Remove null character test due to cr_assert_stdout_eq_str limitations

--- tests/test_char.c
+++ tests/test_char.c
@@ -21,15 +21,6 @@ Test(ft_printf, prints_multiple_characters, .init = setup)
 	cr_assert_eq(len, ex_len);
 }
 
-// Test printing a null character
-Test(ft_printf, prints_null_character, .init = setup)
-{
-	/* Note 1. See at the end of the file */
-    int len = ft_printf("Char: %c\n", '\0');
-	cr_assert_stdout_eq_str("Char: \n");
-	cr_assert_eq(len, 8);
-}
-
 // Test printing a whitespace character
 Test(ft_printf, prints_whitespace, .init = setup)
 {
@@ -80,10 +71,19 @@ Test(ft_printf, prints_extended_ascii_character, .init = setup)
 	cr_assert_eq(len, 8);
 }
 
+/* These tests were disbled because cr_assert_stdout_eq_str 
+	does not properly work with null characters
+
+// Test printing a null character
+Test(ft_printf, prints_null_character, .init = setup)
+{
+    int len = ft_printf("Char: %c\n", '\0');
+	cr_assert_stdout_eq_str("Char: \n");
+	cr_assert_eq(len, 8);
+}
 
 Test(ft_printf, francinette_test_5, .init = setup)
 {
-	/* Note 1. See at the end of the file */
 	int len = ft_printf(" %c %c %c ", '0', 0, '1');
 	cr_assert_stdout_eq_str(" 0  1 ");
 	cr_assert_eq(len, 7);
@@ -91,7 +91,6 @@ Test(ft_printf, francinette_test_5, .init = setup)
 
 Test(ft_printf, francinette_test_8, .init = setup)
 {
-	/* Note 1. See at the end of the file */
 	int len = ft_printf(" %c %c %c ", '2', '1', 0);
 	cr_assert_stdout_eq_str(" 2 1  ");
 	cr_assert_eq(len, 7);
@@ -99,7 +98,6 @@ Test(ft_printf, francinette_test_8, .init = setup)
 
 Test(ft_printf, francinette_test_9, .init = setup)
 {
-	/* Note 1. See at the end of the file */
 	int len = ft_printf(" %c %c %c ", 0, '1', '2');
 	cr_assert_stdout_eq_str("  1 2 ");
 	cr_assert_eq(len, 7);
@@ -107,13 +105,8 @@ Test(ft_printf, francinette_test_9, .init = setup)
 
 Test(ft_printf, francinette_test_category_mix, .init = setup)
 {
-	/* Note 1. See at the end of the file */
 	int len = ft_printf("%%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %%%c%%%s%%%d%%%i%%%u%%%x%%%X%%%% %c%%", 'A', "42", 42, 42 ,42 , 42, 42, 'B', "-42", -42, -42 ,-42 ,-42, 42, 'C', "0", 0, 0 ,0 ,0, 42, 0);
 	cr_assert_stdout_eq_str("%A%42%42%42%42%2a%2A%% %B%-42%-42%-42%4294967254%ffffffd6%2A%% %C%0%0%0%0%0%2A%% %");
 	cr_assert_eq(len, 83);
 }
-
-/* Note 1: can't be tested with sprintf since 
-	the null character stored the string 
-	makes it impossible to compare with 
-	what is printed to stdout */
+*/
